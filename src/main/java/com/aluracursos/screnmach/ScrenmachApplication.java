78c7f0203a5c6732e89ee28ac05a2b2bac21b14f
Raul Blanco <blancogil199@gmail.com>
2025-05-21 18:30:57 -0500

    primera clase Trabajando con lambdas Spring

--- src/main/java/com/aluracursos/screnmach/ScrenmachApplication.java
+++ src/main/java/com/aluracursos/screnmach/ScrenmachApplication.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screnmach;
 
+import com.aluracursos.screnmach.model.DatosSerie;
 import com.aluracursos.screnmach.service.ConsumoAPI;
+import com.aluracursos.screnmach.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,5 +20,9 @@ public class ScrenmachApplication implements CommandLineRunner {
 		var consumoApi = new ConsumoAPI();
 		var json = consumoApi.obtenerDatos( "https://www.omdbapi.com/?t=game+of+thrones&apikey=5523bf38");
 		System.out.println(json);
+
+		ConvierteDatos converson = new ConvierteDatos();
+		var datos = converson.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
 	}
 }
